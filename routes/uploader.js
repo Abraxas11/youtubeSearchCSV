8c6c7c630c266187ed5403102a0fac941254ec5d
George Philippou <gfilip2000@gmail.com>
2020-12-04 16:48:13 +0400

    Added bootstrap

--- routes/uploader.js
+++ routes/uploader.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('This is where you upload the csv');
+    res.render('uploader', { title: "Upload your CSV", message: "Here is where you upload your CSV. " + "\n" + "Up to 100 searches can be made per API key." });
 });
 
 module.exports = router;
